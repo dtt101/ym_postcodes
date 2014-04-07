810dcc4ca02987e96a06535ba22ff48f33e47c5d
David Thompson <dtt101@gmail.com>
2014-04-07 08:36:45 +0100

    use rest-client for requests

--- lib/ym_postcodes.rb
+++ lib/ym_postcodes.rb
@@ -2,5 +2,11 @@ require "ym_postcodes/version"
 require "ym_postcodes/postcode"
 
 module YmPostcodes
-  
+
+  @base_url = 'http://api.postcodes.io'
+
+	class << self
+		attr_accessor :base_url
+	end
+
 end
--- lib/ym_postcodes/postcode.rb
+++ lib/ym_postcodes/postcode.rb
@@ -1,19 +1,30 @@
 require "json"
 
+require "rest_client"
+
 module YmPostcodes
   class Postcode
 
     def self.lookup(postcode)
       safe_postcode = URI.encode(postcode)
-      uri = URI('http://api.postcodes.io/postcodes/' + safe_postcode)
-      response = Net::HTTP.get(uri)
+      self.request('get', "/postcodes/#{safe_postcode}")
+    end
+
+    private
+
+    def self.request(method, path)
+      begin
+        response = RestClient.get "#{YmPostcodes.base_url}#{path}"
+      rescue => e
+        response = e.response
+      end
       JSON.parse(response)
     end
 
   end
 end
 
-# TODO: use RestClient: https://github.com/rest-client/rest-client in postcode.rb
+
 # TODO: setup webmock for tests https://github.com/bblimke/webmock & http://robots.thoughtbot.com/how-to-stub-external-services-in-tests
 # TODO: add batch lookup
 # TODO: look at adding request method with try and exceptions
